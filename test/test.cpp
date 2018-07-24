94b94503521fa62778ce3f65b348c85c72ce5423
Simon Pettersson <simon.x.pettersson@external.atlascopco.com>
2018-07-24 13:48:21 +0200

    Can now both subscribe and write to a connection

--- test/test.cpp
+++ test/test.cpp
@@ -4,6 +4,7 @@
 #include <rxcpp/rx.hpp>
 #include <iostream>
 #include <stdexcept>
+#include <algorithm>
 
 extern "C" {
 #  include <sys/socket.h>
@@ -14,102 +15,164 @@ extern "C" {
 #  include <string.h>
 }
 
-class connection_type {
+#include <rxcpp/rx-includes.hpp>
+
+namespace rxsock {
+
+class socket_t
+{
 public:
-  connection_type() = delete;
-  connection_type(const connection_type&) = delete;
-
-  connection_type(int listen_fd)
-    : client_addr_len{sizeof(client_addr)}
-    , connection_fd{-1}
-    , listen_fd{listen_fd}
-  {}
-
-  connection_type(connection_type&& other)
-    : client_addr{std::move(other.client_addr)}
-    , client_addr_len{std::move(other.client_addr_len)}
-    , connection_fd{std::move(other.connection_fd)}
-    , listen_fd{std::move(other.listen_fd)}
-  {}
-
-  ~connection_type()
+  socket_t(unsigned long port)
   {
-    if(connection_fd >= 0) {
-      ::close(connection_fd);
-      std::cerr << "Connection " << connection_fd << " closed!\n";
+    sockaddr_in serv_addr;
+    {
+      bzero(&serv_addr, sizeof(serv_addr));
+      serv_addr.sin_family = AF_INET;
+      serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+      serv_addr.sin_port = htons(port);
     }
-  }
 
-  void accept()
-  {
-    connection_fd = ::accept(listen_fd, &client_addr, &client_addr_len);
-    if(connection_fd == -1){
+    constexpr auto protocol = 0;
+    fd = socket(AF_INET, SOCK_STREAM, protocol);
+    if(fd == -1) {
+      throw std::runtime_error(strerror(errno));
+    }
+
+    auto iomode = 0; // blocking
+    ioctl(fd, FIONBIO, &iomode);
+
+    if(bind(fd, reinterpret_cast<sockaddr*>(&serv_addr), sizeof(serv_addr))) {
+      throw std::runtime_error(strerror(errno));
+    }
+
+    constexpr auto pool_size = 5;
+    if(listen(fd, pool_size)) {
       throw std::runtime_error(strerror(errno));
     }
   }
 
-  void write(std::vector<char>&& data)
-  {
-    if(::write(connection_fd, data.data(), data.size()) == -1) {
-      throw std::runtime_error(strerror(errno));
-    }
-  }
-
-  std::vector<char> read()
-  {
-    constexpr size_t buffer_size = 1024;
-    std::vector<char> buffer(buffer_size);
-    auto res = ::read(connection_fd, buffer.data(), buffer_size);
-    if(res == -1) {
-      throw std::runtime_error(strerror(errno));
-    }
-    buffer.resize(res);
-    return std::move(buffer);
-  }
+  int file_descriptor() const { return fd; }
 
 private:
-  sockaddr client_addr;
-  socklen_t client_addr_len;
-  int connection_fd;
-  int listen_fd;
+  int fd;
 };
 
-using connection = std::shared_ptr<connection_type>;
+struct connection_t : public rxcpp::sources::source_base<char>
+{
+  struct connection_t_state_type {
+    connection_t_state_type() = delete;
+    connection_t_state_type(const connection_t_state_type&) = delete;
+    connection_t_state_type(connection_t_state_type&&) = delete;
+
+    connection_t_state_type(const socket_t& socket)
+      : client_addr_len{sizeof(client_addr)}
+      , connection_fd{-1}
+      , socket{socket}
+    {}
+
+    ~connection_t_state_type() {
+      if(connection_fd >= 0) {
+        ::close(connection_fd);
+        std::cerr << "Connection " << connection_fd << " closed!\n";
+      }
+    }
+
+    void accept()
+    {
+      connection_fd = ::accept(socket.file_descriptor(), &client_addr, &client_addr_len);
+      if(connection_fd == -1){
+        throw std::runtime_error(strerror(errno));
+      }
+      std::cerr << "Connection established " << connection_fd << '\n';
+    }
+
+    std::vector<char> read()
+    {
+      constexpr size_t buffer_size = 1024;
+      std::vector<char> buffer(buffer_size);
+      auto res = ::read(connection_fd, buffer.data(), buffer_size);
+      if(res == -1) {
+        throw std::runtime_error(strerror(errno));
+      }
+      buffer.resize(res);
+      return std::move(buffer);
+    }
+
+    void write(std::vector<char>&& data) const
+    {
+      if(::write(connection_fd, data.data(), data.size()) == -1) {
+        throw std::runtime_error(strerror(errno));
+      }
+    }
+
+    void close() const {
+      ::close(connection_fd);
+    }
+
+    sockaddr client_addr;
+    socklen_t client_addr_len;
+    int connection_fd;
+    const socket_t& socket;
+  };
+
+  std::shared_ptr<connection_t_state_type> state;
+
+  connection_t(const socket_t& socket)
+    : state{std::make_shared<connection_t_state_type>(socket)}
+  {}
+  connection_t(const connection_t& other)
+    : state{other.state}
+  {}
+  connection_t(connection_t&& other)
+    : state{std::move(other.state)}
+  {}
+
+  template<class Subscriber>
+  void on_subscribe(Subscriber sub) const {
+    static_assert(rxcpp::is_subscriber<Subscriber>::value, "subscribe must be passed a subscriber");
+
+    auto loop = [this, sub] {
+      while(true) {
+        try {
+          auto result = std::move(state->read());
+
+          if(result.size() == 0) {
+            sub.on_completed();
+            return false;
+          }
+
+          auto next = [&](char byte) { sub.on_next(byte); };
+          std::for_each(cbegin(result), cend(result), next);
+        } catch(...) {
+          sub.on_error(std::current_exception());
+          return false;
+        }
+      }
+      return true;
+    };
+    rxcpp::on_exception(loop, sub);
+  }
+};
+
+class connection : public rxcpp::observable<char, connection_t> {
+public:
+  connection(connection_t conn) : conn{conn}, rxcpp::observable<char, connection_t>(conn) {}
+  void write(std::vector<char>&& data) const { conn.state->write(std::forward<std::vector<char>>(data)); }
+  void close() const { conn.state->close(); }
+private:
+  connection_t conn;
+};
 
 auto make_server(unsigned long port) {
   return rxcpp::observable<>::create<connection>
     ([port](rxcpp::subscriber<connection> sub) {
-
-      sockaddr_in serv_addr;
-      {
-        bzero(&serv_addr, sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-        serv_addr.sin_port = htons(port);
-      }
       try {
-        constexpr auto protocol = 0;
-        int listenfd = socket(AF_INET, SOCK_STREAM, protocol);
-        if(listenfd == -1) {
-          throw std::runtime_error(strerror(errno));
-        }
-
-        auto iomode = 0; // blocking
-        ioctl(listenfd, FIONBIO, &iomode);
-
-        if(bind(listenfd, reinterpret_cast<sockaddr*>(&serv_addr), sizeof(serv_addr))) {
-          throw std::runtime_error(strerror(errno));
-        }
-
-        constexpr auto pool_size = 5;
-        if(listen(listenfd, pool_size)) {
-          throw std::runtime_error(strerror(errno));
-        }
+        socket_t socket{port};
 
         while(1) {
-          auto conn = std::make_shared<connection_type>(listenfd);
-          conn->accept();
-          sub.on_next(conn);
+          connection_t conn{socket};
+          conn.state->accept();
+          sub.on_next(connection(conn));
         }
       } catch(...) {
         sub.on_error(std::current_exception());
@@ -120,38 +183,14 @@ auto make_server(unsigned long port) {
     });
 }
 
+}
+
 SCENARIO("hej") {
   using namespace std::chrono_literals;
 
   auto threads = rxcpp::observe_on_event_loop();
 
-  auto socket_connected = [&threads](connection conn) {
-
-    auto read_fn = [conn](auto sub) {
-      while(true) {
-        sub.on_next(std::move(conn->read()));
-      }
-    };
-
-    auto on_read = [](auto data) {
-      std::cout << "READ: ";
-      for(auto& b : data) std::cout << b;
-      std::cout << '\n';
-    };
-
-    auto reader = rxcpp::observable<>::create<std::vector<char>>(read_fn)
-    .subscribe_on(threads)
-    .subscribe(on_read);
-
-    std::cout << "SENDING\n";
-
-    std::vector<char> buffer(1024);
-    time_t ticks = time(nullptr);
-    snprintf(buffer.data(), buffer.size(), "%.24s\r\n", ctime(&ticks));
-    conn->write(std::move(buffer));
-  };
-
-  auto socket_error = [](std::exception_ptr eptr) {
+  auto error = [](std::exception_ptr eptr) {
     try {
       std::rethrow_exception(eptr);
     } catch(const std::exception& e) {
@@ -159,8 +198,30 @@ SCENARIO("hej") {
     }
   };
 
-  make_server(5000)
+  auto socket_connected = [&threads,error](rxsock::connection conn) {
+
+    auto on_read = [](char byte) {
+      std::cout << "Byte: " << byte << '\n';
+    };
+
+    auto echo = [conn](char byte) {
+      std::vector<char> data{byte};
+      conn.write(std::move(data));
+      conn.close();
+    };
+
+    auto on_completed = [] {
+      std::cout << "Socket completed\n";
+    };
+
+    conn
+    .subscribe_on(threads)
+    .tap(echo)
+    .subscribe(on_read, error, on_completed);
+  };
+
+  rxsock::make_server(5000)
     .retry(10)
     .as_blocking()
-    .subscribe(socket_connected, socket_error);
+    .subscribe(socket_connected, error);
 }
